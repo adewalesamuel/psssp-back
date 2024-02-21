740b9b4cdd17d7295f0b1360f6968028ac610a0e
Samuel Adewale <samueladewale@192.168.1.24>
2024-02-21 15:54:42 +0000

    VITE_APP_URL_HOST chnage + front and admin build deploy

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,14 +2,13 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/admin/vite.svg" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PSSSP Admin</title>
-    <script type="module" crossorigin src="/admin/assets/index-b1158dca.js"></script>
-    <link rel="stylesheet" href="/admin/assets/index-df2b4b1a.css">
+    <script type="module" crossorigin src="/assets/index-CYSITAW5.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-DEHpMPok.css">
   </head>
   <body>
     <div id="root"></div>
-    
   </body>
 </html>
