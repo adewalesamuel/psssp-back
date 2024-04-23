9603749e662c7c5c9826bc9f7ad13847ab739426
Samuel Adewale <samueladewale@MacBook-Pro-de-Meraky.local>
2024-04-23 10:07:27 +0000

    front deploy + admin

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,17 +2,14 @@
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/user/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PSSSP</title>
-    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico"> 
+    <link rel="icon" href="/user/favicon.png"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
-    
-
-    <script type="module" crossorigin src="/user/assets/index-2UjrIxcL.js"></script>
-    <link rel="stylesheet" crossorigin href="/user/assets/index-vBeERULD.css">
+    <script type="module" crossorigin src="/user/assets/index-C2ziWlBO.js"></script>
+    <link rel="stylesheet" crossorigin href="/user/assets/index-CsIfERuA.css">
   </head>
   <body>
     <div id="root"></div>
